b969eec8e67605badd017f1b720d4391a3defec3
Zhizhang Deng <reg@2011ysyb.com>
2019-02-21 22:52:57 -0500

    Variable name changes, Implementation of header related functions

--- Asst1/mymalloc.c
+++ Asst1/mymalloc.c
@@ -5,8 +5,8 @@
 static char blocks[4096];   //Will be automatically initialized to 0 since it's static
 
 struct  __attribute__((__packed__)) header {
-	unsigned char isUsed : 1;
-	unsigned char isLarge : 1;
+	unsigned char is_used : 1;
+	unsigned char is_large : 1;
 };
 
 struct __attribute__((__packed__)) header_sm {
@@ -22,16 +22,44 @@ struct __attribute__((__packed__)) header_lg {
 	unsigned char : 2;
 };
 
-void readHeader(void* data, char* isUsed, int* size) {
-
+void read_header(void* data, char* is_used, int* size) {
+	struct header* h = data;
+	*is_used = h->is_used;
+	if(h->is_large) {
+		struct header_lg* h_lg = data;
+		*size = h_lg->size;
+	} else {
+		struct header_sm* h_sm = data;
+		*size = h_sm->size;
+	}
 }
 
-void writeHeader(void* data, char isUsed, int size) {
-
+void write_header(void* data, char is_used, int size) {
+	struct header* h = data;
+	h->is_used = is_used;
+	if(size > 64) {
+		h->is_large = 1;
+	}
+	if(h->is_large) {
+		struct header_lg* h_lg = data;
+		if(size != -1)
+			h_lg->size = size;
+	} else {
+		struct header_sm* h_sm = data;
+		if(size != -1)
+			h_sm->size = size;
+	}
 }
 
-void* getNextHeader(void* currentHeader) {
-	return NULL;
+void* get_next_header(void* current_header) {
+ 	struct header* h = current_header;
+	if(h->is_large) {
+		struct header_lg* h_lg = current_header;
+		return current_header + sizeof(struct header_lg) + h_lg->size;
+	} else {
+		struct header_sm* h_sm = current_header;
+		return current_header + sizeof(struct header_sm) + h_sm->size;
+	}
 }
 
 void* malloc(int size) {
--- Asst1/mymalloc.h
+++ Asst1/mymalloc.h
@@ -12,15 +12,15 @@ void*   mymalloc(int size);
 void    myfree(void* input);
 
 //Internal
-void    readHeader( void* data,     /* IN: location for the header to read */
-                    char* isUsed,   /* OUT: 0 or 1 indicating whether the current block is occupied */
+void    read_header( void* data,     /* IN: location for the header to read */
+                    char* is_used,   /* OUT: 0 or 1 indicating whether the current block is occupied */
                     int* size       /* OUT: current block size in bytes */
                     );
 
-void    writeHeader(    void* data,     /* INOUT: location for the header to be written in */
-                        char isUsed,    /* IN: 0 or 1 indicating whether the current block is occupied */
+void    write_header(    void* data,     /* INOUT: location for the header to be written in */
+                        char is_used,    /* IN: 0 or 1 indicating whether the current block is occupied */
                         int size        /* IN: current block size in bytes [can be -1 indicating don't change the current block's size */
                         );
 
 //return the location for the next header, NULL if not found
-void*   getNextHeader(void* currentHeader /* IN: location for the current header*/);
+void*   get_next_header(void* current_header /* IN: location for the current header*/);
