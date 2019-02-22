33dbf2278dad6a21c19a81a0df505a25c60dd67d
Zhizhang Deng <reg@2011ysyb.com>
2019-02-21 22:14:18 -0500

    mymalloc.c: implemented header structure

--- Asst1/mymalloc.c
+++ Asst1/mymalloc.c
@@ -4,6 +4,24 @@
 
 static char blocks[4096];   //Will be automatically initialized to 0 since it's static
 
+struct  __attribute__((__packed__)) header {
+	unsigned char isUsed : 1;
+	unsigned char isLarge : 1;
+};
+
+struct __attribute__((__packed__)) header_sm {
+	unsigned char : 1;
+	unsigned char : 1;
+	unsigned int size : 6;
+};
+
+struct __attribute__((__packed__)) header_lg {
+	unsigned char : 1;
+	unsigned char : 1;
+	unsigned int size : 12;
+	unsigned char : 2;
+};
+
 void readHeader(void* data, char* isUsed, int* size) {
 
 }
