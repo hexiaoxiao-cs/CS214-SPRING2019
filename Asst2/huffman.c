d802b0612544f077feaaed523657b485e88d1f2d
Xiaoxiao He <xh172@scarletmail.rutgers.edu>
2019-03-29 15:13:33 -0400

    Debugged Huffman

--- Asst2/huffman.c
+++ Asst2/huffman.c
@@ -1,55 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <fcntl.h>	//File system
-#include <dirent.h>	//Directories
+#include <fcntl.h>      //File system
+#include <dirent.h>     //Directories
 
 
-typedef struct node{
+typedef struct node {
 	int count;
-	struct node *left,*right;
+	struct node *left, *right;
 	char* data;
 }node;
 typedef struct MinHeap {
-    int size;
-    struct node** array;
+	int size;
+	struct node** array;
 }MinHeap;
 //HUFFMAN TREE
 node* tree;
-int size=0;
+int size = 0;
 //start to declear functions
-void heapify(MinHeap*,int);
+void heapify(MinHeap*, int);
 node* getMinNodeHeap(MinHeap*);
-void insertNode(MinHeap*,node*);
-MinHeap* initMinHeap(char**,int*,int);
-node** createNodeArray(char**,int*,int);
-void createHuffmanFromFrequency(char** ,int* ,int);
-void createHuffmanFromCodeBook(char**,  char**, int );
-void DongFeng41KuaiDi (node*);
+void insertNode(MinHeap*, node*);
+MinHeap* initMinHeap(char**, int*, int);
+node** createNodeArray(char**, int*, int);
+void createHuffmanFromFrequency(char**, int*, int);
+void createHuffmanFromCodeBook(char**, char**, int);
+void DongFeng41KuaiDi(node*);
 void LaunchDongFengDaoDan();
-void TraverseTreePrefix(char**, char **, char* ,int *, int* , node* );
-void createCodeBook(char** , char **);
+void TraverseTreePrefix(char**, char **, char*, int *, int*, node*);
+void createCodeBook(char**, char **);
 void heapify(MinHeap *heap, int index)
 {
-	int left = index*2+1;
-	int right = index*2+2;
+	int left = index * 2 + 1;
+	int right = index * 2 + 2;
 	node* temp;
-	if(left<heap->size && heap->array[left]-> count < heap -> array[index]->count )
+	if (left<heap->size && heap->array[left]->count < heap->array[index]->count)
 	{
 		//swap the left to the index
-		temp=heap->array[index];
-		heap->array[index]=heap->array[left];
-		heap->array[left]=temp;
+		temp = heap->array[index];
+		heap->array[index] = heap->array[left];
+		heap->array[left] = temp;
 		heapify(heap, left);
 	}
-	if(right<heap->size && heap ->array[right]->count < heap -> array[index] -> count)
+	if (right<heap->size && heap->array[right]->count < heap->array[index]->count)
 	{
 		//swap the right to the index
-		temp = heap -> array[index];
-		heap -> array[index] = heap -> array[right];
-		heap -> array[right] = temp;
+		temp = heap->array[index];
+		heap->array[index] = heap->array[right];
+		heap->array[right] = temp;
 		heapify(heap, right);
 	}
 	return;
@@ -57,141 +57,141 @@ void heapify(MinHeap *heap, int index)
 
 node* getMinNodeHeap(MinHeap *heap)
 {
-	node *toReturn = heap -> array[0];
-	heap -> array[0]= heap -> array[heap -> size-1];
-	heap->size --;
-	heapify(heap,0);
+	node *toReturn = heap->array[0];
+	heap->array[0] = heap->array[heap->size - 1];
+	heap->size--;
+	heapify(heap, 0);
 	return toReturn;
 }
 
-void insertNode(MinHeap *heap,node *toInsert)
+void insertNode(MinHeap *heap, node *toInsert)
 {
 	int curr = heap->size;
-	while(curr!=0 && toInsert->count < heap->array[(curr-1)/2]->count)
+	while (curr != 0 && toInsert->count < heap->array[(curr - 1) / 2]->count)
 	{
-		heap->array[curr]= heap->array[(curr-1)/2];
-		curr=(curr-1)/2;
+		heap->array[curr] = heap->array[(curr - 1) / 2];
+		curr = (curr - 1) / 2;
 	}
-	heap->array[curr]=toInsert;
+	heap->array[curr] = toInsert;
 	heap->size++;
 	return;
 }
 
 MinHeap* initMinHeap(char **contents, int* counts, int many)
 {
-	MinHeap *toReturn = (MinHeap*) malloc (sizeof(MinHeap));
-	toReturn -> size = many;
-	toReturn -> array = createNodeArray(contents, counts, many);
+	MinHeap *toReturn = (MinHeap*)malloc(sizeof(MinHeap));
+	toReturn->size = many;
+	toReturn->array = createNodeArray(contents, counts, many);
 	return toReturn;
 }
 
 /* Creating Node Array
- * Required:
- * Inputs:
- * Char* contents
- * int* counts
- * int many how many entries
- * Sequence: 0 -> inf => least freq -> most freq
- * Outputs:
- * node* head pointer to head
- * Xiaoxiao He 3/12/2019
- */
-node** createNodeArray(char** contents,int* counts,int many)
+* Required:
+* Inputs:
+* Char* contents
+* int* counts
+* int many how many entries
+* Sequence: 0 -> inf => least freq -> most freq
+* Outputs:
+* node* head pointer to head
+* Xiaoxiao He 3/12/2019
+*/
+node** createNodeArray(char** contents, int* counts, int many)
 {
 	node** array = (node**)malloc(sizeof(node*)*many);
-	int i=0;
+	int i = 0;
 	node* temp;
-	size=many;
-	for(i=0;i<many;i++)
+	size = many;
+	for (i = 0; i<many; i++)
 	{
-		temp=(node*)malloc(sizeof(node));
-		temp->count=counts[i];
-		temp->data=contents[i];
-		temp->left=temp->right=NULL;
-		array[i]=temp;
+		temp = (node*)malloc(sizeof(node));
+		temp->count = counts[i];
+		temp->data = contents[i];
+		temp->left = temp->right = NULL;
+		array[i] = temp;
 	}
 	return array;
 }
 
 /* Create Huffman Tree Structure
- * Required Inputs:
- * Char *contents
- * Int* counts
- * int many how many entries
- * Sequence: As before
- * Outputs:
- * Head node pointer to the huffman tree
- */
+* Required Inputs:
+* Char *contents
+* Int* counts
+* int many how many entries
+* Sequence: As before
+* Outputs:
+* Head node pointer to the huffman tree
+*/
 
-void createHuffmanFromFrequency(char** contents,int* counts,int many)
+void createHuffmanFromFrequency(char** contents, int* counts, int many)
 {
-	MinHeap* heap = initMinHeap(contents,counts,many);
-	node *temp1,*temp2,*temp3;
-	while(heap->size!=1)
+	MinHeap* heap = initMinHeap(contents, counts, many);
+	node *temp1, *temp2, *temp3;
+	while (heap->size != 1)
 	{
 		//get two least element from both heap
-		
-		temp1=getMinNodeHeap(heap);
-		temp2=getMinNodeHeap(heap);
-		printf("Merging %s and %s",temp1->data,temp2->data);
-		temp3=(node*) malloc (sizeof(node));
+
+		temp1 = getMinNodeHeap(heap);
+		temp2 = getMinNodeHeap(heap);
+		//printf("Merging %s and %s", temp1->data, temp2->data);
+		temp3 = (node*)malloc(sizeof(node));
 		temp3->left = temp1;
 		temp3->right = temp2;
-		temp3->count = temp1->count+temp2->count;
+		temp3->count = temp1->count + temp2->count;
 		insertNode(heap, temp3);
 	}
-	tree=heap->array[0];
+	tree = heap->array[0];
 	free(heap->array);
 	free(heap);
 	return;
 }
 
 /*
-	createHuffmanFromCodeBook
-	Required
-	Codes ending with \out0
-	words ending with \0
+createHuffmanFromCodeBook
+Required
+Codes ending with \out0
+words ending with \0
 */
 
 void createHuffmanFromCodeBook(char** codes, char** words, int many) {
 	//Initialize huffman tree from codebook
-	int temp=0,cnt=0;
+	int temp = 0, cnt = 0;
 	node* root = (node*)malloc(sizeof(node));
-	root->left=root->right=NULL;
-	node* curr=root;
-	for(temp=0;temp<many;temp++)
+	root->left = root->right = NULL;
+	node* curr = root;
+	for (temp = 0; temp<many; temp++)
 	{
-		cnt=0;
-		curr=root;
-		while(codes[temp][cnt]!='\0')
+		cnt = 0;
+		curr = root;
+		while (codes[temp][cnt] != '\0')
 		{
-			if(codes[temp][cnt]=='0'){
-				if(curr->left==NULL){
-					curr->left=(node*)malloc(sizeof(node));
-					curr->left->left=curr->left->right=NULL;
+			if (codes[temp][cnt] == '0') {
+				if (curr->left == NULL) {
+					curr->left = (node*)malloc(sizeof(node));
+					curr->left->left = curr->left->right = NULL;
 				}
-			curr=curr->left;
+				curr = curr->left;
 			}
-			if(codes[temp][cnt]=='1'){
-				if(curr->right==NULL){
-					curr->right=(node*)malloc(sizeof(node));
-					curr->right->left=curr->right->right=NULL;
+			if (codes[temp][cnt] == '1') {
+				if (curr->right == NULL) {
+					curr->right = (node*)malloc(sizeof(node));
+					curr->right->left = curr->right->right = NULL;
 				}
-			curr=curr->right;
+				curr = curr->right;
 			}
 			cnt++;
 		}
-		curr->data=words[temp];
+		curr->data = words[temp];
 	}
-	tree=root;
+	tree = root;
 	return;
 }
 
 
 //DongFeng-41KuaiDi is an atomic bomb for bombing the global memory -_-
-void DongFeng41KuaiDi (node* curr)
+void DongFeng41KuaiDi(node* curr)
 {
-	if(curr==NULL){return;}
+	if (curr == NULL) { return; }
 	DongFeng41KuaiDi(curr->left);
 	DongFeng41KuaiDi(curr->right);
 	free(curr);
@@ -201,61 +201,63 @@ void DongFeng41KuaiDi (node* curr)
 void LaunchDongFengDaoDan()
 {
 	DongFeng41KuaiDi(tree);
-	size=0;
+	size = 0;
 }
-void TraverseTreePrefix(char** codes, char **words, char* curr,int *nowcode, int* nowword, node* currnode)
+void TraverseTreePrefix(char** codes, char **words, char* curr, int *nowcode, int* nowword, node* currnode)
 {
+	char *stuff;
 	//At the edge which means that must be a value node
-	if(currnode->left==currnode->right==NULL){
-		words[*nowword]=currnode->data;
-		printf("%s\n",currnode->data);
-		printf("%s\n",codes[*nowword]);
-		codes[*nowword]=curr;
-		curr[*nowcode]= '\0';
-		(*nowcode)--;
+	if (currnode->left == NULL &&  currnode->right == NULL) {
+		words[*nowword] = currnode->data;
+		printf("%s\n", currnode->data);
+		curr[*nowcode] = '\0';
+		stuff = (char*)malloc(size * sizeof(char));
+		memcpy(stuff,curr,sizeof(char)*size);
+		codes[*nowword] = stuff;
+		printf("%s\n", codes[*nowword]);
 		(*nowword)++;
 		return;
 	}
 	//Left Node
 	{
 		//Begin Accessing
+
+		curr[*nowcode] = '0';
 		(*nowcode)++;
-		curr[*nowcode]='0';
-		TraverseTreePrefix(codes,words,curr,nowcode,nowword,currnode->left);
+		TraverseTreePrefix(codes, words, curr, nowcode, nowword, currnode->left);
 		//Finished Accessing--Cleaning
 		(*nowcode)--;
-		curr[*nowcode]='\0';
+		curr[*nowcode] = '\0';
 	}
 	//Right Node
 	{
 		//Begin Accessing
+
+		curr[*nowcode] = '1';
 		(*nowcode)++;
-		curr[*nowcode]='1';
-		TraverseTreePrefix(codes,words,curr,nowcode,nowword,currnode->right);
+		TraverseTreePrefix(codes, words, curr, nowcode, nowword, currnode->right);
 		//Finished Accessing--Cleaning
 		(*nowcode)--;
-		curr[*nowcode]='\0';
+		curr[*nowcode] = '\0';
 	}
 	return;
 }
 /* Create Huffman Codebook From Huffman Tree
-	 Required:
-	 Executed createHuffmanFromFrequency
-	 tree Not NULL
-	 Return:
-	 Codes: Encoded Huffman Codes
-	 Words: The corrosponding Words
-	 size: The size of the Array
-	 Rule:
-	 prefix Traverse
+Required:
+Executed createHuffmanFromFrequency
+tree Not NULL
+Return:
+Codes: Encoded Huffman Codes
+Words: The corrosponding Words
+size: The size of the Array
+Rule:
+prefix Traverse
 */
 void createCodeBook(char** codes, char **words) {
-	char* curr = (char*) malloc (sizeof(char)*size);
-	int nowcode=0,nowword=0;
-	if(tree==NULL) return; //For security, check whether there is a Huffman Tree
-	codes = (char**) malloc (sizeof(char*)*size);
-	words = (char**) malloc (sizeof(char*)*size);
-	TraverseTreePrefix(codes,words,curr, &nowcode,&nowword,tree);
+	char* curr = (char*)malloc(sizeof(char)*size);
+	int nowcode = 0, nowword = 0;
+	if (tree == NULL) return; //For security, check whether there is a Huffman Tree
+	TraverseTreePrefix(codes, words, curr, &nowcode, &nowword, tree);
 }
 
 //FILE SYSTEM RELATED STUFF
@@ -267,31 +269,37 @@ void createCodeBook(char** codes, char **words) {
 //5. decompress according to constructed tree
 
 /* HuffmanCodeBook Format
- *
- * <ASCII bytestring> <\t> <token> <\n>
- * ...
- * ...
- * terminated with new line(\n)
- *
- */
+*
+* <ASCII bytestring> <\t> <token> <\n>
+* ...
+* ...
+* terminated with new line(\n)
+*
+*/
 
 //Test Driver For Xiaoxiao He's Huffman Tree
 
 void main()
 {
-	char *a[3];
-	a[0]="abc";
-	a[1]="bcd";
-	a[2]="hahaha";
-	int i =0;
-	char **b,**e;
-	int c[3]={123,234,345};	
-	createHuffmanFromFrequency(a,c,3);
-	createCodeBook(b,e);
-	if(tree==NULL){printf("NULL TREE");return;}
-	for(i=0;i<3;i++)
+	char *a[6];
+	a[0] = "a";
+	a[1] = "dog";
+	a[2] = "cat";
+	a[3] = "button";
+	a[4] = "tall";
+	a[5] = "and";
+	int i = 0;
+	char **b, **e;
+	int c[6] = { 5,9,12, 13 ,16,45 };
+	createHuffmanFromFrequency(a, c, 6);
+	size = 6;
+	b = (char**)malloc(sizeof(char*)*size);
+	e = (char**)malloc(sizeof(char*)*size);
+	createCodeBook(b, e);
+	if (tree == NULL) { printf("NULL TREE"); return; }
+	for (i = 0; i<6; i++)
 	{
-		printf("%s\t%s\n",b[i],e[i]);
+		printf("%s\t%s\n", b[i], e[i]);
 	}
 
 
@@ -309,15 +317,17 @@ void readFile(const char* file_path, char** data, int* size) {
 	int tmp, ret;
 	int file_size = lseek(handler, 0, SEEK_END);
 	char* huge_shit = (char*)malloc(file_size + 1);
-	huge_shit[file_size] = 0;	//Zero terminated
+	huge_shit[file_size] = 0;       //Zero terminated
 	tmp = 0;
-	while(tmp < file_size) {
+	while (tmp < file_size) {
 		ret = read(handler, huge_shit + tmp, file_size - tmp);
-		if(ret < 0) {
+		if (ret < 0) {
 			//TODO error checking
-		} else if(ret == 0) {
+		}
+		else if (ret == 0) {
 			break;
-		} else {
+		}
+		else {
 			//Positive interger
 			tmp += ret;
 		}
