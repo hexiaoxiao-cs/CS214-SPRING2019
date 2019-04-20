5d83cd20e20160698d4b7b7059adbd3a9fb1971b
Xiaoxiao He <xh172@scarletmail.rutgers.edu>
2019-04-19 22:53:29 -0400

    Finished Implementation of RW_LOCK with hashmap

--- Asst3/server/ds.c
+++ Asst3/server/ds.c
@@ -1,3 +1,33 @@
 #include "ds.h"
-
+#include <pthread.h>
+#include <search.h>
 static pthread_mutex_t hashmap_mtx = PTHREAD_MUTEX_INITIALIZER; //mutex for hashmap (project - rwlock)
+
+//Initialize Hash Map
+//Return 0 indicating success
+//Otherwise indicating not success
+
+int init_hashmap()
+{
+    return hcreate(10000);
+}
+
+//Get RW Lock
+//Input: Project Name
+//Output: a read_write lock for the specified project name
+
+pthread_rwlock_t* get_rwlock_for_project(const char* project_name)
+{
+    ENTRY e,*ep;
+    ACTION act;
+
+    e.key=project_name;
+    ep=hsearch(e);
+    if(ep->data==NULL){
+        if(pthread_rwlock_init(ep->data,pthread_rwlockattr_setkind_np())!=0){return NULL;}
+        else {
+            hsearch(ep);
+            return ep->data;
+        }
+    }
+}
