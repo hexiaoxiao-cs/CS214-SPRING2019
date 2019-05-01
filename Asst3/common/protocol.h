b9d2665367900429fecf0dce8b016b86cbe67518
Zhizhang Deng <andy@dzz007.com>
2019-04-30 23:22:38 -0400

    Some interface change to protocol layer

--- Asst3/common/protocol.h
+++ Asst3/common/protocol.h
@@ -7,6 +7,17 @@
 #include <stddef.h>
 #include <stdint.h>
 
+/*
+ * STRUCTURE NOTE:
+ *
+ * FOR SERVER:
+ *      NETWORK LAYER (INITIATOR) -> PROTOCOL LAYER -> LOGICAL LAYER -> RETURN TO NETWORK LAYER
+ *
+ * FOR CLIENT:
+ *      LOGICAL LAYER (INITIATOR) -> NETWORK LAYER -> RETURN TO LOGICAL LAYER -> PROTOCOL LAYER (TOOL) -> RETURN TO LOGICAL LAYER
+ *
+ */
+
 typedef struct {
     size_t payload1_size;
     const char* payload1;
@@ -40,32 +51,78 @@ typedef struct {
 
 #if defined(SERVER_COMPILING)
 /*
+ * give control to protocol layer, and generate a network response
+ *
  * return val:
- *  0: set out_packet already
+ *  0: out_packet has been set
  *  1: close connection without responding
  *
  * in_packet:
- *  a pointer to a full packet received by network
+ *  a pointer to a full packet received by network, this will be freed by protocol layer when able
  *
  * out_packet:
  *  a pointer pointing to a pointer that should be set to the pointer to desired,
- *  response_packet buffer when return value is 0
+ *  out_packet will be set when return value is 0
+ *
  */
 int process_packet(buffer* in_packet, buffer** out_packet);
 
+/*
+ * API for logic layer to free underlying input packet after finish using parsed_request_t
+ */
 void free_in_packet();
+
+/*
+ * This function need to be called by logical layer after it has set its first payload (if using two payloads response)
+ * If called with only one payload's response is UB
+ */
 void finalize_file_payload1_for_response(buffer* buf);
-void finalize_buffer_for_response(buffer* buf);
 
 #elif defined (CLIENT_COMPILING)
+
+/*
+ * This function need to be called by logical layer after it has set its first payload in the buffer (if using two payloads response)
+ * If called with only one payload's response is UB
+ */
 void finalize_file_payload1_for_request(buffer* buf);
-void finalize_buffer_for_request(buffer* buf);
+
 #endif
 
-parsed_response_t parse_response(buffer* in_packet);
-parsed_request_t parse_request(buffer* in_packet);
-buffer* get_output_buffer_for_request(uint8_t op_code, const char* project_name, uint8_t project_name_size);
+
+/*
+ * Utility function to parse a response buffer into parsed_response_t
+ *
+ * return val:
+ *  0: parse success
+ *  1: malformed packet
+ *
+ * SUGGESTION: use stack space to hold out
+ */
+int parse_response(buffer* in_packet, parsed_response_t* out);
+
+/*
+ * Utility function to parse a request buffer into parsed_request_t
+ * return val:
+ *  0: parse success
+ *  1: malformed packet
+ *
+ * SUGGESTION: use stack space to hold out
+ */
+int parse_request(buffer* in_packet, parsed_request_t* out);
+
+/*
+ * Generate a buffer to be used as a request buffer
+ */
+buffer* get_output_buffer_for_request(uint8_t op_code, const char* project_name, size_t project_name_size, uint8_t is_two_payload);
+
+/*
+ * Generate a buffer to be used as a response buffer
+ */
 buffer* get_output_buffer_for_response(uint16_t status_code, uint8_t is_two_payload);
 
+/*
+ * This function need to be called by logical layer after it has set all of its payload
+ */
+void finalize_buffer_for(buffer* buf);
 
 #endif
