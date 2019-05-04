6c912150c39b102a5beee8b113983cc2b004b010
Zhizhang Deng <andy@dzz007.com>
2019-05-04 16:06:37 -0400

    Updated readme, updated comments inside util.h, pump libtar version

--- Asst3/common/util.h
+++ Asst3/common/util.h
@@ -35,8 +35,16 @@ typedef struct{
     int many_Items;
 } project;
 
+
+/*
+ *  =============================================
+ *
+ *  EXPANDABLE BUFFER UTILITY
+ *
+ *  =============================================
+ */
+
 buffer* createBuffer();
-int isDir(const char *name);
 void    destroyBuffer(buffer* space);
 void    destroyBufferWithoutFree(buffer* space);
 void    expandBuffer(buffer* space, size_t size);
@@ -48,24 +56,19 @@ size_t  getLengthBuffer(buffer *ptr);
 void    appendSequenceBuffer(buffer* space, const char* sequence, size_t sequence_size);
 buffer* duplicateBuffer(buffer* space);
 void    zeroUnusedBuffer(buffer* space);
-int writeFile(const char *file_path, char *data, size_t size);
-int readFile(char *filename, char** buffer,size_t *size);
-int readManifest(char* manifest_raw,size_t size, project* curr_project);
-int writeManifest(char** manifest_towrite,project *curr_project,int old_new);
-void sort_manifest(manifest_item** items, size_t len);
-int compareManifest(int isTwoManifest, manifest_item** client_side, manifest_item** server_side, manifest_item*** changelog,manifest_item*** conflicts, size_t size_client, size_t size_server, int client_ver, int server_ver);
-int proecessManifest_ByChangelist_Push(project* manifest,manifest_item** changelist, size_t changelist_size);
-int proecessManifest_ByChangelist_Update(project* manifest,manifest_item** changelist, size_t changelist_size);
-unsigned char *base64_decode(const char *data,
-                             size_t input_length,
-                             size_t *output_length);
-char *base64_encode(const unsigned char *data,
-                    size_t input_length,
-                    size_t *output_length);
+void    appendBuffer(buffer *space, char c);
 
-int tar_extract_specific_file(const char* tar_file, const char* stored_filename,
-                              const char* output_root_path);
+/*
+ *  =============================================
+ *
+ *  FILE SYSTEM UTILITY
+ *
+ *  =============================================
+ */
 
+int     isDir(const char *name);
+int     writeFile(const char *file_path, char *data, size_t size);
+int     readFile(char *filename, char** buffer,size_t *size);
 /*
  * Utility to sanitize input path (relative)
  *
@@ -131,7 +134,50 @@ char* sanitize_path(const char* input_path);
  *  return value: see sanitize_path
  */
 char* is_valid_path(const char* input_path);
+
+
+/*
+ *  =============================================
+ *
+ *  MANIFEST PROCESSING UTILITY
+ *
+ *  =============================================
+ */
+
+int readManifest(char* manifest_raw,size_t size, project* curr_project);
+int writeManifest(char** manifest_towrite,project *curr_project,int old_new);
+void sort_manifest(manifest_item** items, size_t len);
+int compareManifest(int isTwoManifest, manifest_item** client_side, manifest_item** server_side, manifest_item*** changelog,manifest_item*** conflicts, size_t size_client, size_t size_server, int client_ver, int server_ver);
+int proecessManifest_ByChangelist_Push(project* manifest,manifest_item** changelist, size_t changelist_size);
+int proecessManifest_ByChangelist_Update(project* manifest,manifest_item** changelist, size_t changelist_size);
 int readChangeLogFile(manifest_item ***changelog,char **input,size_t size, size_t *list_size);
 int writeChangeLogFile(manifest_item **changelog,char** output,size_t size,int type);
-void appendBuffer(buffer *space, char c);
+
+
+/*
+ *  =============================================
+ *
+ *  ENCODING UTILITY
+ *
+ *  =============================================
+ */
+
+unsigned char *base64_decode(const char *data,
+                             size_t input_length,
+                             size_t *output_length);
+char *base64_encode(const unsigned char *data,
+                    size_t input_length,
+                    size_t *output_length);
+
+/*
+ *  =============================================
+ *
+ *  TAR EXTRACTION UTILITY
+ *
+ *  =============================================
+ */
+
+int tar_extract_specific_file(const char* tar_file, const char* stored_filename,
+                              const char* output_root_path);
+
 #endif
